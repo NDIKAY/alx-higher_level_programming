e22364b8d725b2953a1d9f16b45316bf676c2086
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-11-28 23:21:07 +0200

    Inventing is a combination of brains and materials

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for x in range(10):
     for y in range(10):
-        if(x < y):
-            print("{}{}".format(x, y), end=", " if("{}{} < 89".format(x, y)) else "\n")
-    
-
-
+        if (x < y):
+            print("{}{}".format(x, y), end=", " if  x != 8 else "\n")
