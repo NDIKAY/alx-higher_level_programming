9042f6ac76a1b3e34ba255894d7b86c962e425f9
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-11-29 03:16:10 +0200

    print power number

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-    return(result)
+    if b > 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+    else:
+        result = 1.0
+        for _ in range(abs(b)):
+            result /= a
+    return result
