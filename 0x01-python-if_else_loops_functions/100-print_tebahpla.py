72c58977e91857044db89180dd0b258252dc0f4a
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-11-29 11:41:43 +0200

    reversed string alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-alphabet ='abcdefghijklmnopqrstuvwxyz'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 reversed_alphabet = alphabet[::-1]
 for i in range(len(reversed_alphabet)):
     char = reversed_alphabet[i]
-    if i % 2 == 0:
+    if i % 2 != 0:
         char = char.upper()
     print("{}".format(char), end="")
-    
