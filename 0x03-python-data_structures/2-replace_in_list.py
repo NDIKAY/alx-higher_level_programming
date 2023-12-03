d30f268c15b79a1989cc4f32479c2a280bd39317
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-12-03 23:35:30 +0200

    2. Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if edx < 0 or edx >= len(my_list:
+        return my_list
     if 0 < idx <= len(my_list):
         my_list[idx] = element
         return my_list
