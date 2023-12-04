3b5a73632329791624bbba14a488c287bdcce272
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-12-04 19:47:54 +0200

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     tuple_mine =()
     if len(sentence) == 0:
-        tuple_mine = 0, "None"
+        tuple_mine = 0, None
     else:
         tuple_mine = len(sentence), sentence[0]
     return tuple_mine
