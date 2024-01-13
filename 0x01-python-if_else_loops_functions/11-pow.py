3be3d4591ba43f95c2885ff29d81479e9f0b8e51
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2024-01-13 07:13:39 +0200

    11. a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b > 0:
-        result = 1
-        for _ in range(b):
-            result *= a
-    else:
-        result = 1.0
-        for _ in range(abs(b)):
-            result /= a
-    return result
+    return (a ** b)
