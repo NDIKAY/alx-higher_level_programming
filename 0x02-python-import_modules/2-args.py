6851b50ba4e29fbcb27f6c496a5048b413fd1e1e
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2024-01-14 07:05:43 +0200

    Corrected output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     print("{} argument{}{}".format(
         arglen - 1,
         's' if arglen != 2 else '',
-        '' if arglen == 1 else ':'
+        ' ' if arglen == 1 else ':'
         ))
     for i in range(1, arglen):
         print("{}: {}".format(i, argv[i]))
