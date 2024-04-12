1d48e5205b5ac2e143a26c92b05f2c9b2f92c24d
NDIKUBWIMANA Balthazar <nbarthazar80@gmail.com>
2024-04-12 09:13:58 -0700

    python-network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to make a request to 0.0.0.0:5000/catch_me to receive a response with th
-curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+# This script sends a request to 0.0.0.0:5000/catch_me and receives a response with "You got me!" in the body.
+curl -s -L -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 54.209.116.64:5000/catch_me
