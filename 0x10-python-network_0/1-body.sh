3fb93a32c8a9ee1e219543a2c5eb0d2cb3ba9af2
NDIKUBWIMANA Balthazar <nbarthazar80@gmail.com>
2024-04-12 07:16:13 -0700

    python-Server

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to fetch the body of a 200 status code response from a URL, following any redirections
-curl -s -L "$1" | grep -o '<body.*' | tail -n +1
+# Script to fetch the body of a 200 status code response from a URL, handling redirections
+curl -s -L "$1" -o response.tmp && grep "HTTP/1.1 200 OK" response.tmp && cat response.tmp
