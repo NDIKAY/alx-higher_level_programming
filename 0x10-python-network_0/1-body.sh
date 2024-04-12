47525100b3a1a2df27ee0594a7c00b3e0986986a
NDIKUBWIMANA Balthazar <nbarthazar80@gmail.com>
2024-04-12 06:13:32 -0700

    PYthon-Network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to fetch the body of a 200 status code response from a URL
-curl -s -o body.tmp -w "%{http_code}" "$1" | grep -q "200" && cat body.tmp
+curl -s -L -w "%{http_code}" -o /dev/null "$1" | grep -q "200" && curl -s "$1"
