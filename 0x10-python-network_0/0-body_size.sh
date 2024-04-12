1e5a53aa7f83813a77ebd661c8d983074fff246e
NDIKUBWIMANA Balthazar <nbarthazar80@gmail.com>
2024-04-12 05:21:54 -0700

    Python-network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script to fetch the body of the response from a URL and display its size in bytes
+# Script to fetch thie body of the response from a URL and display its size in bytes
 curl -s "$1" | wc -c
-
